528b546d93445fed83e9c5a163ec7dfe9e631b7c
Jonathan Picazo <macbethjonathan@gmail.com>
2019-07-16 18:08:28 -0400

    add initial react client setup

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root';
+
+import './reset.css';
+import './styles.css';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
