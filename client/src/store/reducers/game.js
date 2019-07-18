8018bceff3cee75dc08f0ec02625a9c87e1dced3
Jonathan Picazo <macbethjonathan@gmail.com>
2019-07-18 12:19:20 -0400

    add conway lifecycle logic

--- client/src/store/reducers/game.js
+++ client/src/store/reducers/game.js
@@ -6,7 +6,8 @@ import {
   GAME_FASTER,
   GAME_SLOWER,
   GAME_RESET,
-  TOGGLE_CELL
+  TOGGLE_CELL,
+  APPLY_LIFECYCLE
 } from '../constants';
 
 const initialState = {
@@ -14,35 +15,72 @@ const initialState = {
   generation: 0,
   grid: Array(25 * 25).fill(false),
   running: false,
-  speed: 0
+  speed: 1000,
+  timer: null
 };
 
 export const gameReducer = produce((draft, action) => {
   switch (action.type) {
     case GAME_START:
       draft.running = true;
+      draft.timer = setInterval(() => {
+        console.log('tick');
+      }, draft.speed);
       draft.clickable = false;
       return;
     case GAME_STOP:
+      clearInterval(draft.timer);
+      draft.timer = null;
       draft.running = false;
       return;
     case GAME_FASTER:
-      draft.speed++;
+      draft.speed -= 50;
+      if (draft.speed < 200) draft.speed = 200;
       return;
     case GAME_SLOWER:
-      draft.speed--;
+      draft.speed += 50;
+      if (draft.speed > 1000) draft.speed = 1000;
       return;
     case GAME_RESET:
         draft.clickable = true;
         draft.generation = 0;
         draft.grid = Array(25 * 25).fill(false);
         draft.running = false;
-        draft.speed = 0;
+        draft.speed = 1000;
+        clearInterval(draft.timer);
+        draft.timer = null;
         return;
     case TOGGLE_CELL:
       draft.grid[action.payload] = !draft.grid[action.payload];
+      return;
+    case APPLY_LIFECYCLE:
+      localStorage.setItem('generate', false);
+      draft.generation++;
+      for (let i = 0; i < draft.grid.length; i++)
+        draft.grid[i] = lifecycle(draft.grid, i);
       return;
     default:
       return;
   }
 }, initialState);
+
+function lifecycle(grid, idx) {
+  const state = [
+    grid[idx-25], // top
+    grid[idx+25], // bottom
+    grid[idx-1],  // left
+    grid[idx+1],  // right
+    grid[idx-26], // top left
+    grid[idx-24], // top right
+    grid[idx+24], // bottom left
+    grid[idx+26]  // bottom right
+  ];
+
+  const alive = state.reduce((acc, cur) => cur ? acc + 1 : acc, 0);
+
+  if (grid[idx] && (alive < 2 || alive >= 4)) {
+    return false;
+  } else if (!grid[idx] && alive === 3) {
+    return true;
+  }
+}
