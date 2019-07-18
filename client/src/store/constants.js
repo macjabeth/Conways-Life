935b9075b0eda0e9e724fbf3047269cff8559d61
Jonathan Picazo <macbethjonathan@gmail.com>
2019-07-18 17:41:08 -0400

    remove superfluous conflux data

--- client/src/store/constants.js
+++
@@ -1,8 +0,0 @@
-export const GAME_START = 'GAME_START';
-export const GAME_STOP = 'GAME_STOP';
-export const GAME_FASTER = 'GAME_FASTER';
-export const GAME_SLOWER = 'GAME_SLOWER';
-export const GAME_RESET = 'GAME_RESET';
-
-export const TOGGLE_CELL = 'TOGGLE_CELL'
-export const APPLY_LIFECYCLE = 'APPLY_LIFECYCLE';
